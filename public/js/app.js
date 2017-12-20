4d7bddb30fb7fa3c512587764a07324ddbf38cfe
atebit <chris@adjustcreative.com>
2017-12-19 18:14:13 -0600

    add a possible workaround for double click zoom safari

--- public/js/app.js
+++ public/js/app.js
@@ -125,8 +125,30 @@ var ASSETS_LOADED = true;
 
 
 if(MOBILE){
+// // TODo: add this function instaed of the touchend one below..
+// (function($) {
+//   $.fn.nodoubletapzoom = function() {
+//       $(this).bind('touchstart', function preventZoom(e) {
+//         var t2 = e.timeStamp
+//           , t1 = $(this).data('lastTouch') || t2
+//           , dt = t2 - t1
+//           , fingers = e.originalEvent.touches.length;
+//         $(this).data('lastTouch', t2);
+//         if (!dt || dt > 500 || fingers > 1) return; // not double-tap
 
-  $('nozoom').bind('touchend', function(e) { e.preventDefault(); $(this).click(); });
+//         e.preventDefault(); // double tap - prevent the zoom
+//         // also synthesize click events we just swallowed up
+//         $(this).trigger('click').trigger('click');
+//       });
+//   };
+// })(jQuery);
+
+
+
+  $('nozoom').bind('touchend', function(e) { 
+    e.preventDefault(); 
+    $(this).click(); 
+  });
   // hide the browser chrome on mobile
   // When ready...
   window.addEventListener("load",function() {
